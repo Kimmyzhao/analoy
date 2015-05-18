e7d6db3997ad74691bf4daf88c9db2a19fd71639
kimmyzhao <cj901218zsy@163.com>
2015-05-18 13:27:07 +0800

    update

--- src/main/java/cn/epalmpay/analoy/scheduling/PullTradesRecord.java
+++ src/main/java/cn/epalmpay/analoy/scheduling/PullTradesRecord.java
@@ -1,18 +1,16 @@
 package cn.epalmpay.analoy.scheduling;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 import cn.epalmpay.analoy.service.qiandaibao.QiandaibaoService;
 
-@Component
+//@Component
 public class PullTradesRecord {
 
 	@Autowired
 	private QiandaibaoService qiandaibaoService;
 
-	@Scheduled(cron = "0 0/10 * * * ?")
+	// @Scheduled(cron = "0 0/10 * * * ?")
 	public void task() {
 		qiandaibaoService.getTradeRecord();
 	}
